fd496bfffa00136818338d55e5311a5cd156d1aa
Sargam <16co26@aitdgoa.edu.in>
2019-07-15 15:54:22 +0530

    First C Program

---
+++ Exp1/exp1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int i;
+	char str[i];
+	printf("Enter length of string: ");
+	scanf("%d",&i);
+	printf("Enter the string:  ");
+	scanf("%s",&str[i]);
+	for(i=0;i<=str[i];i++){
+		if(str[i]=='a'||str[i]=='b'||str[i]=='c'||str[i]=='d'||str[i]=='e'||str[i]=='f'||str[i]=='g'||str[i]=='h'||str[i]=='i'||str[i]=='j'||str[i]=='k'||str[i]=='l'||str[i]=='m'||str[i]=='n'||str[i]=='o'||str[i]=='p'||str[i]=='q'||str[i]=='r'||str[i]=='s'||str[i]=='t'||str[i]=='u'||str[i]=='v'||str[i]=='w'||str[i]=='x'||str[i]=='y'||str[i]=='z'){
+			printf("char-->%s",str[i]);
+		}
+		else if(str[i]=='0'||str[i]=='1'||str[i]=='2'||str[i]=='3'||str[i]=='4'||str[i]=='5'||str[i]=='6'||str[i]=='7'||str[i]=='8'||str[i]=='9'){
+			printf("char-->%s",str[i]);
+		}
+	}
+	return 0;
+}
